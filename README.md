1c2b583c00e57514b0f988cbc103c6591feee280
mervin <mydiamervin@gmail.com>
2020-06-25 13:17:46 +0800

    update

--- README.md
+++ README.md
@@ -1,30 +1,22 @@
 # console_colorpoint
 
-### version
-
-1.0.7
-
-优化了类型判断，以及显示效果
-
-### introducation
+## introducation
 
 一个console库，使用不同的颜色标记console输出，更加醒目，轻松注释掉所有console，发布无忧。
 
-### 使用
+## 使用
 
 import Log from 'console_colorpoint'
 
-const log = new Log()
+const log = new Log({})
 
-log.green({name: 'Jack'})
+log.g({name: 'Jack'})
 
-#### 构造函数
+### 构造函数
 
-Log生成实例时传入任意隐式转换为 `true` 的值都会让所有 `console` 失效。
+Log生成实例时传入 `false` 让所有 `console` 失效。
 
-### todo
-
-Set等可遍历的支持table展示 // 完成
+## todo
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,82 @@
-class ColorLog  {
-    constructor (x) {
-        this._x = !x;
-        return new Proxy(this, {
-            get(target, key, receiver) {
-                target[`${key}`] = (...ag) => {
-                    return target._x ? _log(`${key}`)(...ag) : noDo
-                }
-                return Reflect.get(target, key, receiver);
-            },
-            set: function (target, key, value, receiver) {
-                console.log(`${key}`)
-                return Reflect.set(target, key, value, receiver);
-            }
+const opt = {
+    fontSize: 16,
+    //red
+    s: 'salmon',
+    e: 'crimson',
+    r: 'red',
+    // pink
+    p: 'pink',
+    h: 'hotPink',
+    // orange
+    j: 'coral',
+    t: 'tomato',
+    o: 'orange',
+    // yellow
+    g: 'gold',
+    y: 'yellow',
+    k: 'khaki',
+    // purple
+    q: 'lavender',
+    v: 'violet',
+    f: 'fuchsia',
+    p: 'purple',
+    i: 'indigo',
+    // green
+    l: 'lime',
+    s: 'springGreen',
+    g: 'green',
+    u: 'olive',
+    t: 'teal',
+    // blue
+    c: 'cyan',
+    a: 'aquamarine',
+    b: 'blue',
+    n: 'navy',
+    // Brown
+    m: 'maroon',
+    x: 'chocolate',
+    // Gray
+    z: 'gray',
+    // black
+    d: 'dark',
+    // white
+    w: 'white'
+}
+
+class ColorLog {
+    constructor(x) {
+        // this._x = x;
+        const _opt = { ...opt, ...x }
+        const _arr = Object.keys(_opt)
+        _arr.forEach(e => {
+            this[e] = (...ag) => _log(_opt[e])(...ag)
         })
+
+        // new Proxy(this, {
+        //     get(target, key, receiver) {
+        //         if (Reflect.ownKeys(_opt).includes(key)) {
+        //             target[`${key}`] = (...ag) => {
+        //                 return x ? _log(_opt[key])(...ag) : noDo
+        //             }
+        //         }
+        //         return Reflect.get(target, key, receiver);
+        //     },
+        //     set: function (target, key, value, receiver) {
+        //         return Reflect.set(target, key, value, receiver);
+        //     }
+        // })
     }
 }
 
-function noDo (...args) {
+// const map = new Map([])
+
+function noDo(...args) {
     return undefined;
 }
 
 function getType(obj) {
     var type = Object.prototype.toString.call(obj).match(/^\[object (.*)\]$/)[1].toLowerCase();
-    if(type === 'string' && typeof obj === 'object') return 'object'; // Let "new String('')" return 'object'
+    if (type === 'string' && typeof obj === 'object') return 'object'; // Let "new String('')" return 'object'
     if (obj === null) return 'null'; // PhantomJS has type "DOMWindow" for null
     if (obj === undefined) return 'undefined'; // PhantomJS has type "DOMWindow" for undefined
     return type;
@@ -30,35 +84,58 @@ function getType(obj) {
 
 function _log(color) {
     return (...args) => {
-        const fontSize = 16
-        console.group(color)
-        args.forEach((e,i) => {
+        const fontSize = 12
+        const fontSize_big = fontSize + 2
+        const fontSize_small = fontSize - 2
+        const _len = args.length
+        if (_len > 1) {
+            console.group(color)
+        }
+        args.forEach((e, i) => {
             const type = getType(e)
-            if(['array', 'set'].includes(type)) {
-                console.log(`%c${type}:`, `font-size:${fontSize}px;color:${color};padding:0 ${fontSize}px;border-right:${fontSize}px solid ${color};border-left:${fontSize}px solid ${color}`)
-                console.table(Array.from(e))
-            } else {
-                console.log(`%c${type}:`, `font-size:${fontSize}px;color:${color};padding:0 ${fontSize}px;border-right:${fontSize}px solid ${color};border-left:${fontSize}px solid ${color}`)
-                console.log(e)
+            if (_len > 1) {
+                console.log(`%c* 第${i}个参数:`, `font-size:${fontSize_small}px;color:darkred;`)
             }
-            if(i < args.length - 1) {
-                console.log(`%c<=|=|=|=|=|=| Next Param |=|=|=|=|=|=>`, `font-size:${fontSize}px;color:${color};`)
+            // console.log(`%c${e}%c<${type}>`, `font-size:${fontSize}px;color:${color};`, `font-size:${fontSize_small}px;color:${color};`)
+            switch (type) {
+                // base
+                case 'number':
+                case 'string':
+                case 'boolean':
+                case 'undefined':
+                case 'null':
+                // special
+                case 'regexp':
+                case 'date':
+                    console.log(`${_len > 1 ? '  ' : ''} %c${e} %c${type}`, `font-size:${fontSize}px;color:${color};`, `font-size:${fontSize_small}px;color:#fff;background: #000;padding: 2px 4px;border-radius: 2px;`)
+                    break;
+                // 
+                case 'object':
+                case 'function':
+                case 'array':
+                case 'map':
+                case 'set':
+                    console.log(`${_len > 1 ? '  ' : ''}`, e)
+                    console.log(`%c${type}`, `font-size:${fontSize_small}px;color:#fff;background: #000;padding: 2px 4px;border-radius: 2px;`)
+                    break;
             }
+            // if (['array', 'set'].includes(type)) {
+            //     console.log(`%c${type}:`, `font-size:${fontSize}px;color:${color};`)
+            //     console.table(Array.from(e))
+            //     console.log(`%c${type}:`, `font-size:${fontSize}px;color:${color};`)
+            // } else {
+            // console.log(`%c${type}:${e}`, `font-size:${fontSize}px;color:${color};`)
+            // }
+            // if (i < args.length - 1) {
+            //     console.log(`%c第${i}个参数`, `font-size:${fontSize_small}px;background:${color};color:#fff`)
+            // }
         })
-        console.groupEnd()
+        if (_len > 1) {
+            console.groupEnd(color)
+        }
     }
 }
 
 
-
-// bind
-// const _bind = function (content) {
-//     var _this = this;
-//     return function () {
-//         _this.apply(content, arguments)
-//     }
-// }
-
-// Function.prototype._bind = _bind
 
 export default ColorLog
